8c13d9836d39a9824807baf4b80c6a68a8637e29
Sir-josh <docsmile01@gmail.com>
2026-07-16 14:42:46 +0100

    configure the backend URL to commuicate with the vercel frontend URL

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
 // Detect if the app is currently running on your local machine
-// const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
-// Automatically points to your live Render server or local fallback
-// const BASE_URL = isLocalhost 
-//     ? 'http://localhost:5000/api' 
-//     : 'https://clinicalmanagementsystem-2ojw.onrender.com/api'; // Added /api here!
 
 const api = axios.create({
     baseURL: BASE_URL,
